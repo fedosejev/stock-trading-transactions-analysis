2df931cbbda4b3b5f3262e2ef947b262e755cb8b
Alex Bush <alex.v.bush@gmail.com>
2016-04-09 15:22:31 -0700

    Add unitests and refactor app.js to use new engine object for profit-loss calculation

--- source/js/engine.js
+++ source/js/engine.js
@@ -1,4 +1,13 @@
-function calculateProfitsOrLossesForEachStock(stocks) {
+function Engine(currencySign, commission) {
+  this.currencySign = currencySign;
+  this.commission = commission;
+}
+
+Engine.prototype.calculateProfitsOrLossesForEachStock = function(stocks) {
+  var self = this;
+
+  console.log("stocks input format");
+  console.log(stocks);
 
   var overallSellOutcome = 0;
   var stocksSells = [];
@@ -80,7 +89,7 @@ function calculateProfitsOrLossesForEachStock(stocks) {
         for (var i = 0; i < buyTransactions.length; i++) {
           if (buyTransactions[i].numberOfShares === quantitySold) {
 
-            console.debug('Case: =');
+            console.log('Case: =');
             console.log('Need to sell:', quantitySold);
 
             var previousSellOutcome = sellOutcome;
@@ -89,14 +98,14 @@ function calculateProfitsOrLossesForEachStock(stocks) {
 
             console.log('Actually sold:', quantitySold);
 
-            console.debug((sellOutcome - previousSellOutcome).toFixed(2));
+            console.log((sellOutcome - previousSellOutcome).toFixed(2));
 
             buyTransactions[i].numberOfShares = buyTransactions[i].numberOfShares - quantitySold;
             break;
 
           } else if (buyTransactions[i].numberOfShares < quantitySold) {
 
-            console.debug('Case: <');
+            console.log('Case: <');
             console.log('Need to sell:', quantitySold);
 
             quantitySold = quantitySold - buyTransactions[i].numberOfShares;
@@ -107,13 +116,13 @@ function calculateProfitsOrLossesForEachStock(stocks) {
 
             sellOutcome = sellOutcome + ((parseFloat(transaction['Net value']) / parseInt(transaction['Quantity'])) * buyTransactions[i].numberOfShares) - (buyTransactions[i].pricePaidPerShare * buyTransactions[i].numberOfShares);
 
-            console.debug((sellOutcome - previousSellOutcome).toFixed(2));
+            console.log((sellOutcome - previousSellOutcome).toFixed(2));
 
             buyTransactions[i].numberOfShares = 0;
 
           } else if (buyTransactions[i].numberOfShares > quantitySold) { 
 
-            console.debug('Case: >');
+            console.log('Case: >');
             console.log('Need to sell:', quantitySold);
 
             var previousSellOutcome = sellOutcome;
@@ -122,11 +131,11 @@ function calculateProfitsOrLossesForEachStock(stocks) {
 
             console.log('Actually sold:', quantitySold);
 
-            console.debug((sellOutcome - previousSellOutcome).toFixed(2));
+            console.log((sellOutcome - previousSellOutcome).toFixed(2));
 
             buyTransactions[i].numberOfShares = buyTransactions[i].numberOfShares - quantitySold;
             break;
-          
+
           }
         }
 
@@ -139,30 +148,204 @@ function calculateProfitsOrLossesForEachStock(stocks) {
       }
     });
 
-    console.debug('\n------------------');
+    console.log('\n------------------');
     if (sellTransactions.length > 0) {
-      console.debug('💰 Final result for ' +  STOCK_SYMBOL + ': ' + CURRENCY_SIGN + finalSellOutcome.toFixed(2));
+      console.log('💰 Final result for ' +  STOCK_SYMBOL + ': ' + self.currencySign + finalSellOutcome.toFixed(2));
     } else {
-      console.debug('💰 No final result for ' +  STOCK_SYMBOL + ', because you didn\'t sell ' +  STOCK_SYMBOL + ' yet.');
+      console.log('💰 No final result for ' +  STOCK_SYMBOL + ', because you didn\'t sell ' +  STOCK_SYMBOL + ' yet.');
     }
-    console.debug('------------------');
+    console.log('------------------');
 
     overallSellOutcome = overallSellOutcome + finalSellOutcome;
 
     stockSells.overallResult = finalSellOutcome.toFixed(2);
-    stockSells.commissions = stockTransactions.length * COMMISSION;
+    stockSells.commissions = stockTransactions.length * self.commission;
     stockSells.overallResult = stockSells.overallResult - stockSells.commissions;
 
     stocksSells.push(stockSells);
   });
 
-  console.debug('\n\n=====================================================');
-  console.debug('💰💰💰 Your overall trading results: ' + CURRENCY_SIGN + overallSellOutcome.toFixed(2));
-  console.debug('=====================================================\n\n\n');
+  var totalCommissions = stocksSells.reduce(function sumOutcomes(accumulator, currentValue) {
+    return parseFloat(accumulator) + parseFloat(currentValue.commissions);
+  }, 0);
 
-  return stocksSells;
-}
+  console.log('\n\n=====================================================');
+  console.log('💰💰💰 Your overall trading results: ' + self.currencySign + overallSellOutcome.toFixed(2));
+  console.log('=====================================================\n\n\n');
 
-module.exports = {
-  calculateProfitsOrLossesForEachStock: calculateProfitsOrLossesForEachStock
+  return { stockSells: stocksSells, overallSellOutcome: overallSellOutcome - totalCommissions, totalCommissions: totalCommissions };
 };
+
+module.exports = Engine;
+
+// function calculateProfitsOrLossesForEachStock(stocks) {
+
+//   console.log("stocks input format");
+//   console.log(stocks);
+
+//   var overallSellOutcome = 0;
+//   var stocksSells = [];
+
+//   stocks.forEach(function calculateForStock(stockTransactions) {
+
+//     var STOCK_SYMBOL = stockTransactions[0]['Stock code'];
+
+//     var stockSells = {
+//       symbol: STOCK_SYMBOL,
+//       sells: []
+//     };
+
+//     console.log('\n\n\n\n🔮 Calculating ' + STOCK_SYMBOL + '\n\n');
+
+//     var totalPaidForAllShares = 0;
+//     var numberOfShares = 0;
+//     var minimumSharePricePaid = 0;
+
+//     var buyTransactions = [];
+//     var sellTransactions = [];
+
+//     var finalSellOutcome = 0;
+
+//     stockTransactions.forEach(function calculateForTransaction(transaction) {
+//       if (transaction['Type'] === 'Bought') {
+
+//         console.log('=== Bought ===');
+
+//         var netValuePaid = parseFloat(transaction['Net value']);
+//         var quantityBought = parseInt(transaction['Quantity']);
+//         var pricePaidPerShare = netValuePaid / quantityBought;
+
+//         var boughtTransaction = {
+//           netValuePaid: netValuePaid,
+//           numberOfShares: quantityBought,
+//           pricePaidPerShare: pricePaidPerShare,
+//           date: transaction['Date']
+//         };
+
+//         console.log(boughtTransaction);
+
+//         buyTransactions.push(boughtTransaction);
+
+//       } else if (transaction['Type'] === 'Sold') {
+
+//         console.log('=== Sold ===');
+
+//         var netValuePaid = parseFloat(transaction['Net value']);
+//         var quantityBought = parseInt(transaction['Quantity']);
+//         var pricePaidPerShare = netValuePaid / quantityBought;
+
+//         var soldTransaction = {
+//           netValuePaid: netValuePaid,
+//           numberOfShares: quantityBought,
+//           pricePaidPerShare: pricePaidPerShare,
+//           date: transaction['Date']
+//         };
+
+//         console.log(soldTransaction);
+
+//         sellTransactions.push(soldTransaction);
+
+//         buyTransactions.sort(function compare(a, b) {
+//           if (a.pricePaidPerShare < b.pricePaidPerShare) {
+//             return -1;
+//           }
+
+//           if (a.pricePaidPerShare > b.pricePaidPerShare) {
+//             return 1;
+//           }
+
+//           return 0;
+//         });
+
+//         var quantitySold = parseInt(transaction['Quantity']);
+//         var sellOutcome = 0;
+
+//         for (var i = 0; i < buyTransactions.length; i++) {
+//           if (buyTransactions[i].numberOfShares === quantitySold) {
+
+//             console.log('Case: =');
+//             console.log('Need to sell:', quantitySold);
+
+//             var previousSellOutcome = sellOutcome;
+
+//             sellOutcome = sellOutcome + (parseFloat(transaction['Net value']) / parseInt(transaction['Quantity']) * quantitySold) - buyTransactions[i].netValuePaid;
+
+//             console.log('Actually sold:', quantitySold);
+
+//             console.log((sellOutcome - previousSellOutcome).toFixed(2));
+
+//             buyTransactions[i].numberOfShares = buyTransactions[i].numberOfShares - quantitySold;
+//             break;
+
+//           } else if (buyTransactions[i].numberOfShares < quantitySold) {
+
+//             console.log('Case: <');
+//             console.log('Need to sell:', quantitySold);
+
+//             quantitySold = quantitySold - buyTransactions[i].numberOfShares;
+
+//             console.log('Actually sold:', buyTransactions[i].numberOfShares);
+
+//             var previousSellOutcome = sellOutcome;
+
+//             sellOutcome = sellOutcome + ((parseFloat(transaction['Net value']) / parseInt(transaction['Quantity'])) * buyTransactions[i].numberOfShares) - (buyTransactions[i].pricePaidPerShare * buyTransactions[i].numberOfShares);
+
+//             console.log((sellOutcome - previousSellOutcome).toFixed(2));
+
+//             buyTransactions[i].numberOfShares = 0;
+
+//           } else if (buyTransactions[i].numberOfShares > quantitySold) { 
+
+//             console.log('Case: >');
+//             console.log('Need to sell:', quantitySold);
+
+//             var previousSellOutcome = sellOutcome;
+
+//             sellOutcome = sellOutcome + ((parseFloat(transaction['Net value']) / parseInt(transaction['Quantity'])) * quantitySold) - (buyTransactions[i].pricePaidPerShare * quantitySold);
+
+//             console.log('Actually sold:', quantitySold);
+
+//             console.log((sellOutcome - previousSellOutcome).toFixed(2));
+
+//             buyTransactions[i].numberOfShares = buyTransactions[i].numberOfShares - quantitySold;
+//             break;
+
+//           }
+//         }
+
+//         finalSellOutcome = finalSellOutcome + sellOutcome;
+
+//         stockSells.sells.push({
+//           date: transaction['Date'],
+//           result: finalSellOutcome
+//         });
+//       }
+//     });
+
+//     console.log('\n------------------');
+//     if (sellTransactions.length > 0) {
+//       console.log('💰 Final result for ' +  STOCK_SYMBOL + ': ' + CURRENCY_SIGN + finalSellOutcome.toFixed(2));
+//     } else {
+//       console.log('💰 No final result for ' +  STOCK_SYMBOL + ', because you didn\'t sell ' +  STOCK_SYMBOL + ' yet.');
+//     }
+//     console.log('------------------');
+
+//     overallSellOutcome = overallSellOutcome + finalSellOutcome;
+
+//     stockSells.overallResult = finalSellOutcome.toFixed(2);
+//     stockSells.commissions = stockTransactions.length * COMMISSION;
+//     stockSells.overallResult = stockSells.overallResult - stockSells.commissions;
+
+//     stocksSells.push(stockSells);
+//   });
+
+//   console.log('\n\n=====================================================');
+//   console.log('💰💰💰 Your overall trading results: ' + CURRENCY_SIGN + overallSellOutcome.toFixed(2));
+//   console.log('=====================================================\n\n\n');
+
+//   return stocksSells;
+// }
+
+// module.exports = {
+//   calculateProfitsOrLossesForEachStock: calculateProfitsOrLossesForEachStock
+// };
---
+++ spec/engine/engine_spec.js
@@ -0,0 +1,55 @@
+describe("Engine", function() {
+  var Engine = require('../../source/js/engine');
+
+  var engine;
+
+  beforeEach(function() {
+    engine = new Engine("$", 10);    
+  });
+
+  describe("#calculateProfitsOrLossesForEachStock", function() {
+
+    describe("given valid input", function(){
+
+      beforeEach(function(){
+
+        // TODO: replace test objects data genration with a factory like https://github.com/rosiejs/rosie
+
+        var teslaBuyTransaction = {
+          "Date": "01/11/2015",
+          "Net value": 982.22,
+          "Quantity": 7,
+          "Stock code": "TSLA",
+          "Type": "Bought",
+        };
+
+        var teslaSellTransaction = {
+          "Date": "01/12/2015",
+          "Net value": 1289.25,
+          "Quantity": 7,
+          "Stock code": "TSLA",
+          "Type": "Sold",
+        };
+
+        var teslaTransactions = [teslaBuyTransaction, teslaSellTransaction];
+
+        this.input = [teslaTransactions];
+      });
+
+      it("should return the right result", function(){        
+        expect(engine.calculateProfitsOrLossesForEachStock(this.input)).not.toBeNull();
+
+        var result = engine.calculateProfitsOrLossesForEachStock(this.input);
+
+        expect(result.overallSellOutcome).toEqual(287.03);
+        expect(result.totalCommissions).toEqual(20);
+      });
+    });
+
+    describe("given invalid input", function(){
+      it("should throw an error");
+    });
+
+  });
+
+});
