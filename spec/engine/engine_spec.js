f4097a8329ae3e42c85e1af864663de4608cb105
Alex Bush <alex.v.bush@gmail.com>
2016-04-09 15:51:49 -0700

    Better spec sample for profit-loss calculation

--- spec/engine/engine_spec.js
+++ spec/engine/engine_spec.js
@@ -15,25 +15,61 @@ describe("Engine", function() {
 
         // TODO: replace test objects data genration with a factory like https://github.com/rosiejs/rosie
 
-        var teslaBuyTransaction = {
+        // var teslaBuyTransaction = {
+        //   "Date": "01/11/2015",
+        //   "Net value": 982.22,
+        //   "Quantity": 7,
+        //   "Stock code": "TSLA",
+        //   "Type": "Bought",
+        // };
+
+        // var teslaSellTransaction = {
+        //   "Date": "01/12/2015",
+        //   "Net value": 1289.25,
+        //   "Quantity": 7,
+        //   "Stock code": "TSLA",
+        //   "Type": "Sold",
+        // };
+
+        // var teslaTransactions = [teslaBuyTransaction, teslaSellTransaction];
+
+        // this.input = [teslaTransactions];
+
+        var buyTransaction1 = {
           "Date": "01/11/2015",
-          "Net value": 982.22,
-          "Quantity": 7,
-          "Stock code": "TSLA",
+          "Net value": 906.79,
+          "Quantity": 2,
+          "Stock code": "AMZN",
           "Type": "Bought",
         };
 
-        var teslaSellTransaction = {
+        var buyTransaction2 = {
           "Date": "01/12/2015",
-          "Net value": 1289.25,
-          "Quantity": 7,
+          "Net value": 2268.33,
+          "Quantity": 6,
+          "Stock code": "AMZN",
+          "Type": "Bought",
+        };
+
+        var buyTransaction3 = {
+          "Date": "01/13/2015",
+          "Net value": 1764.69,
+          "Quantity": 5,
+          "Stock code": "AMZN",
+          "Type": "Bought",
+        };
+
+        var sellTransaction4 = {
+          "Date": "01/14/2015",
+          "Net value": 4802.43,
+          "Quantity": 12,
           "Stock code": "TSLA",
           "Type": "Sold",
         };
 
-        var teslaTransactions = [teslaBuyTransaction, teslaSellTransaction];
-
-        this.input = [teslaTransactions];
+        var transactions = [buyTransaction1, buyTransaction2, buyTransaction3, sellTransaction4];
+  
+        this.input = [transactions];
       });
 
       it("should return the right result", function(){        
@@ -41,8 +77,8 @@ describe("Engine", function() {
 
         var result = engine.calculateProfitsOrLossesForEachStock(this.input);
 
-        expect(result.overallSellOutcome).toEqual(287.03);
-        expect(result.totalCommissions).toEqual(20);
+        expect(result.overallSellOutcome.toFixed(1)).toEqual(276.015.toFixed(1));
+        expect(result.totalCommissions).toEqual(40);
       });
     });
 
