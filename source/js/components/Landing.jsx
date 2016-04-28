42b6c536a5d50b97965970e2d3fe4828c2f3522f
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2016-04-28 19:36:02 +0100

    Update

--- source/js/components/Landing.jsx
+++ source/js/components/Landing.jsx
@@ -17,7 +17,7 @@ var Landing = React.createClass({
 
               <h2 className="how-to-use">Drag and drop your CSV file exported from the Robinhood app here.</h2>
 
-              <h3>This app calculates your profits and/or losses from your stock trading. We <strong>do not</strong> save your data.</h3>
+              <h3>StockTrading.report calculates profits/losses from your stock trading using Robinhood app. We <strong>do not</strong> save your data.</h3>
 
               <footer>Created by <a href="http://artemij.com">Artemij Fedosejev</a> in 2016.</footer>
             </section>
