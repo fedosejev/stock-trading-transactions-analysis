ef841ee73d73239af97bb18a8baf392ee566f7ca
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2016-05-03 05:26:25 +0100

    Update

--- source/js/components/RobinhoodLoginForm.jsx
+++ source/js/components/RobinhoodLoginForm.jsx
@@ -31,7 +31,7 @@ var RobinhoodLoginForm = React.createClass({
           <input type="password" className="form-control input-lg" id="passwordInput" required ref="password" />
         </div>
 
-        <p className="not-saving-login-message">We <strong>do not</strong> store your username and/or password. This is an open source project - you're welcome to inspect the source code <a href="https://github.com/fedosejev/using-robinhood-node-api-client" target="_blank">here</a>.</p>
+        <p className="not-saving-login-message">We <strong>do not</strong> store your username and/or password. This is an open source project - you're welcome to inspect the source code <a href="https://github.com/fedosejev/stock-trading-transactions-analysis" target="_blank">here</a>.</p>
 
         <div className="submit-button-container">
           <button type="submit" className="btn btn-success btn-lg">Show my stock trading results</button>
